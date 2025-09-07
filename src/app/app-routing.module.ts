7f2c589a2b42ff1ba167659789c9861d94c49551
Lakshmi <lakshminarayana.chilukuru@gmail.com>
2025-09-07 23:04:46 +0530

    bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+const repo2Url ="http://localhost:4210/remoteEntry.js"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:AppComponent
+  },
+  {
+    path:'nav-list',
+    loadChildren: ()=>{
+      return loadRemoteModule({
+        remoteName:"repo2",
+        remoteEntry : repo2Url,
+        exposedModule : './NavListModule'
+      })
+      .then(m=>m.NavListModule)
+      .catch(err=>console.log(err))
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
